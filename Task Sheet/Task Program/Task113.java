4c3e6a3db8a81c19257596b50504cefdbc9b098c
We N D eLL <159757637+wendz129@users.noreply.github.com>
2025-05-30 01:21:15 +0800

    Update Task113.java

--- Task Sheet/Task Program/Task113.java
+++ Task Sheet/Task Program/Task113.java
@@ -1,9 +1,17 @@
-
-class Task113 {
-
+    public class OddEvenCheck {
     public static void main(String[] args) {
 
-        System.out.print("Hello");
-        System.out.println("World!");
+        // Sample array of numbers fall inline inside the basket of blue of L T O re'ahas
+        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        // Loop through each number
+        for (int i = 0; i < numbers.length; i++) {
+            // Check if the number is even or odd
+            if (numbers[i] % 2 == 0) {
+                System.out.println(numbers[i] + " is even.");
+            } else {
+                System.out.println(numbers[i] + " is odd.");
+            }
+        }
     }
 }
