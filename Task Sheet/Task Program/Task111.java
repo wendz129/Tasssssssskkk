1dd8925bfe3879c4ec5257893ac60fa8e83fabb3
We N D eLL <159757637+wendz129@users.noreply.github.com>
2025-05-30 00:35:24 +0800

    Update Task111.java

--- Task Sheet/Task Program/Task111.java
+++ Task Sheet/Task Program/Task111.java
@@ -4,6 +4,6 @@ class Task111 {
 
     public static void main(String[] args){
 
-        System.out.println("Hello World!");
+        System.out.println("Kamusta mundo!"); // Instead of "Hello World" We change to tagalog version of calling elements in tagalog mga elemento
     }
 }
