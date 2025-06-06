f147d3547b65be3fafeb939e6d432a63c9b9b422
We N D eLL <159757637+wendz129@users.noreply.github.com>
2025-06-06 12:17:31 +0800

    Add files via upload

---
+++ LoginForm/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    // this class file is for ConsoleExample
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Hoopla Comics!"); // Prints "Hello Hoopla Comics" to the console
+        System.out.println("The answer is: " + 2 + 2); // Prints "The answer is: 4"
+
+    }
+}
