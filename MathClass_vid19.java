652d208bf4ced8372cbfb9e3b5ecc4af1e868faa
We N D eLL <159757637+wendz129@users.noreply.github.com>
2025-06-06 12:57:09 +0800

    Add files via upload

---
+++ MathClass_vid19.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class MathClass_vid19{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter radius : ");
+        int a = scanner.nextInt();
+
+        System.out.println("Area of circle is : "+ Math.PI * a*a);
+
+    }
+
+}
