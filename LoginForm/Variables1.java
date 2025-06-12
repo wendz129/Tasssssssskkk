cb081ae8e912ad5def244fdf91cb32aadfd461fa
We N D eLL <159757637+wendz129@users.noreply.github.com>
2025-06-12 20:42:10 +0800

    Add files via upload

---
+++ LoginForm/Variables1.java
@@ -0,0 +1,24 @@
+public class Variables1 {
+    public static void main(String[] args) {
+        // Declare and initialize variables here
+
+        int age = 27;
+        int temperature = -5;
+        int count = 100;
+        int quantity = 5;
+
+        double price = 99.99;
+        double pi = 3.14159;
+        double fraction = 0.5;
+        double itemPrice = 24.99;
+        
+        
+        // Output the values
+        // System.out.println("Quantity: " + quantity);
+        // System.out.println("Price: " + itemPrice);
+
+        System.out.println("Your age is: " + age);
+        System.out.println("Fraction: " + fraction);
+        
+    }
+}
