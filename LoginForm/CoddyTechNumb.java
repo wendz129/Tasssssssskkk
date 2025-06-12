c52304a5fe2e4e4d72b0cc2f3d5b0af3d428957a
We N D eLL <159757637+wendz129@users.noreply.github.com>
2025-06-12 23:34:43 +0800

    Add files via upload

---
+++ LoginForm/CoddyTechNumb.java
@@ -0,0 +1,22 @@
+public class CoddyTechNumb {
+    public static void main(String[] args) {
+        // Create integer variables
+        int temperature = 28;
+        int humidity = 65;
+        int windSpeed = 15;
+        
+        // Create decimal variables
+        double morningTemp = 22.5;
+        double aftenoonTemp = 28.8;
+        double eveningTemp = 23.4;
+        
+        
+        // Don't modify the code below
+        System.out.println("Current Temperature: " + temperature);
+        System.out.println("Humidity Level: " + humidity);
+        System.out.println("Wind Speed: " + windSpeed);
+        System.out.println("Morning Temperature: " + morningTemp);
+        System.out.println("Afternoon Temperature: " + afternoonTemp);
+        System.out.println("Evening Temperature: " + eveningTemp);
+    }
+}
