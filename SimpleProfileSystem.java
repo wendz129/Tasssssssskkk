87d0d3c2589000836dae3ef47a8474dbcf77286f
We N D eLL <159757637+wendz129@users.noreply.github.com>
2025-06-13 15:01:26 +0800

    Add files via upload

---
+++ SimpleProfileSystem.java
@@ -0,0 +1,21 @@
+public class SimpleProfileSystem { 
+    //You're creating a simple profile system. This several String variables to store different pieces of information
+    public static void main(String[] args) {
+        // Create your String variables below
+        
+        String firstName = "Wendell";
+        String city = "New York";
+        String hobby = "Playing guitar";
+        String greeting = "Hello, nice to meet you!";
+        String favoriteQuote = "Life is like a box of chocolates";
+        String mood = "Happy";
+
+        // Don't modify the code below
+        System.out.println("Name: " + firstName);
+        System.out.println("City: " + city);
+        System.out.println("Hobby: " + hobby);
+        System.out.println("Greeting: " + greeting);
+        System.out.println("Favorite Quote: " + favoriteQuote);
+        System.out.println("Current Mood: " + mood);
+    }
+}
