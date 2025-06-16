b6067cfb2e7c2d4856082deba609b017c753726d
We N D eLL <159757637+wendz129@users.noreply.github.com>
2025-06-16 20:12:09 +0800

    Add files via upload

---
+++ LoginForm/CharvarGameSymbol.java
@@ -0,0 +1,22 @@
+public class CharvarGameSymbol {
+    public static void main(String[] args) {
+        // Create your char variables below
+
+        char player = '+';
+        char enemy = '*';
+        char coin = '$';
+        char heart = '3';
+        char arrow = '>';
+        char wall = '#';
+        
+        // Don't modify the code below
+        System.out.println("Game Symbols:");
+        System.out.println("-------------");
+        System.out.println("Player: " + player);
+        System.out.println("Enemy: " + enemy);
+        System.out.println("Coin: " + coin);
+        System.out.println("Life: " + heart);
+        System.out.println("Direction: " + arrow);
+        System.out.println("Wall: " + wall);
+    }
+}
---
+++ LoginForm/TypeDeclaration.java
@@ -0,0 +1,20 @@
+public class TypeDeclaration {
+    public static void main(String[] args) {
+        // Declare variables here // Declare the variables with their corresponding types and values
+        
+        int count = 10;
+        double total = 150.75;
+        char grade = 'A';
+        boolean isActive = false;
+        String userName = "Wendell";
+
+        // Output the values
+        System.out.println("Count: " + count);
+        System.out.println("Total: " + total);
+        System.out.println("Grade: " + grade);
+        System.out.println("Active: " + isActive);
+        System.out.println("User name: " + userName);
+
+        
+    }
+}
