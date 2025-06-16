c45556c9ca5dec8ba3f107ad428eec1b4e3fcb21
We N D eLL <159757637+wendz129@users.noreply.github.com>
2025-06-16 21:39:03 +0800

    Add files via upload

---
+++ LoginForm/Variables part2/RecapInitializeVar.java
@@ -0,0 +1,13 @@
+public class RecapInitializeVar {
+    public static void main(String[] args) {
+        // Type your code below
+        int k = 88;
+        final double PI = 3.14;
+        String name = "Wendell";
+
+        //Don't change the line below
+        System.out.println("k = " + k);
+        System.out.println("PI = " + PI);
+        System.out.println("name = " + name);
+    }
+}
