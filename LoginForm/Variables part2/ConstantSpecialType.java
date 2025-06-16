f641f231fa56ba3a44b448c07791fde105e9cfde
We N D eLL <159757637+wendz129@users.noreply.github.com>
2025-06-16 20:47:40 +0800

    Add files via upload

---
+++ LoginForm/Variables part2/ConstantSpecialType.java
@@ -0,0 +1,12 @@
+public class ConstantSpecialType {
+    public static void main(String[] args) {
+        // Type your code below
+        final double PI = 3.14159;
+        // PI = 3.14159 // Attempting to change the value of constant will result in an error
+
+
+        // Don't change the line below
+        System.out.println("PI = " + PI);
+        
+    }
+}
